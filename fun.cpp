3a9824d8fd2e6e8166a3113a6b6236d694b2cd1b
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-07-07 17:20:33 +1200

    modified:   Pentagram 	modified:   fun.cpp 	modified:   res/shaders/fragment.glsl 	modified:   res/shaders/vertex.glsl

--- fun.cpp
+++ fun.cpp
@@ -1,4 +1,5 @@
 #include <Pentagram.hpp>
+#include <iostream>
 
 PNT::image image;
 
@@ -16,7 +17,7 @@ int main(int argc, char *argv[]) {
     }
 
     image.load("res\\textures\\logo\\ghoul.png");
-    PNT::Window window("Drag'n Drop", image.getWidth(), image.getHeight(), 500, 500, ImGuiConfigFlags_ViewportsEnable | ImGuiConfigFlags_DockingEnable);
+    PNT::Window window("Drag'n Drop", image.getWidth(), image.getHeight(), 500, 500, ImGuiConfigFlags_None);
     image.loadOnGPU();
     window.setEventCallback(eventCallback);
 
@@ -31,17 +32,47 @@ int main(int argc, char *argv[]) {
     fragmentShader.compile();
 
     // Shader program.
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader.getID());
-    glAttachShader(shaderProgram, fragmentShader.getID());
-    glLinkProgram(shaderProgram);
+    PNT::program shader({&vertexShader, &fragmentShader});
+    shader.link();
+    std::cout << shader.getError(600) << '\n';
+
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f
+    };
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glUniform1i(glGetUniformLocation(shader.getID(), "texture"), image.getID());
 
     while(!window.shouldClose()) {
         PNT::processEvents();
 
         window.startFrame();
-        image.ImGuiDraw(ImVec2(500, 500));
+
+        ImGui::SetNextWindowSizeConstraints(ImVec2(image.getWidth() + 2.0f, image.getHeight() + 35.0f), ImVec2(image.getWidth() + 2.0f, image.getHeight() + 35.0f));
+        ImGui::Begin("Image");
+        image.ImGuiDraw(ImVec2(image.getWidth(), image.getHeight()));
+        ImGui::End();
 
         window.endFrame();
     }
--- res/shaders/fragment.glsl
+++ res/shaders/fragment.glsl
@@ -1,7 +1,10 @@
 #version 330 core
 out vec4 FragColor;
 
-void main()
-{
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+in vec2 TexCoord;
+
+uniform sampler2D texture;
+
+void main() {
+	FragColor = texture2D(texture, TexCoord);
 }
